007a16a9ee17ef4f86e8411050860887a3fb689a
Lishijie-1993 <250569674@qq.com>
2025-08-17 21:06:15 +0800

    更改头像

--- blog.config.js
+++ blog.config.js
@@ -16,7 +16,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'NotionNext', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '一个普通的干饭人🍚', // 作者简介
-  AVATAR: 'https://skillful-legal-51c.notion.site/image/attachment%3Ab2125777-a38c-4014-84e2-faed1d6447d8%3AL21_2695.jpg?table=block&id=25266703-d1ad-808c-8790-ca6be6df1981&spaceId=40166703-d1ad-81ff-8d0b-00030a1e0cd0&width=1420&userId=&cache=v2',
+  AVATAR: 'https://i.imgur.com/56FCkvN.jpeg',
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://tangly1024.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
