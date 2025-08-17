3d68b377375d2a349a9227b8effb8b7ce4c63f7d
Lishijie-1993 <250569674@qq.com>
2025-08-17 19:56:12 +0800

    更改了个人照片链接

--- blog.config.js
+++ blog.config.js
@@ -16,6 +16,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'NotionNext', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '一个普通的干饭人🍚', // 作者简介
+  AVATAR: 'https://file.notion.so/f/f/40166703-d1ad-81ff-8d0b-00030a1e0cd0/ab93b482-7022-44c2-81f7-e1f8fbfe35dd/L21_2695.jpg?table=block&id=25266703-d1ad-80c4-8fc4-c0e7ea9c8af3&spaceId=40166703-d1ad-81ff-8d0b-00030a1e0cd0&expirationTimestamp=1755475200000&signature=NwRxRJF5qhwJVZv7z22XMBWYYr5L4_C5QDJDUlQATCg&downloadName=L21_2695.jpg',
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://tangly1024.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
